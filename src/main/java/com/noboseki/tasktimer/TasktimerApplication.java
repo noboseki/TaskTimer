8680bf2010477e147c7a80dbff40dd7782d6e3b2
Ragnar <wit.dawid@interia.pl>
2020-09-23 17:11:32 +0200

    first commi

---
+++ src/main/java/com/noboseki/tasktimer/TasktimerApplication.java
@@ -0,0 +1,13 @@
+package com.noboseki.tasktimer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TasktimerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TasktimerApplication.class, args);
+    }
+
+}
