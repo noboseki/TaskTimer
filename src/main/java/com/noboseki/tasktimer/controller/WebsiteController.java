874ca49e76a1cf8bed81eaec6d7dfb35b7991fec
Ragnar <wit.dawid@interia.pl>
2020-11-14 17:55:32 +0100

    Add main page

---
+++ src/main/java/com/noboseki/tasktimer/controller/WebsiteController.java
@@ -0,0 +1,17 @@
+package com.noboseki.tasktimer.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping
+public class WebsiteController {
+
+    @GetMapping("/")
+    public String mainPage() {
+        return "index";
+    }
+}
