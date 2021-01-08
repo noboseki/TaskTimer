c7b6256dc4dc1eca22bf1181e72ce1190b89721f
DawidW1993 <wit.dawid@interia.pl>
2021-01-08 14:09:20 +0100

    Add side menu and remove Thymeleaf tags from HTML

---
+++ src/main/resources/static/js/navigation.js
@@ -0,0 +1,9 @@
+let sideMenu = document.getElementById("sideMenu");
+
+function opernSideBar() {
+    sideMenu.style.transform = "translate(221px, 0)";
+}
+
+function closeSideBar() {
+    sideMenu.style.transform = "translate(-221px, 0)";
+}
