87bdddd0c2d17891844d42c772f826667c9dc58c
DawidW1993 <wit.dawid@interia.pl>
2021-01-15 14:18:06 +0100

    Create service util Test

--- src/main/java/com/noboseki/tasktimer/playload/SessionServiceTableByDateResponse.java
+++ src/main/java/com/noboseki/tasktimer/playload/SessionServiceTableByDateResponse.java
@@ -3,11 +3,13 @@ package com.noboseki.tasktimer.playload;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 import java.time.LocalDate;
 
 @Getter
 @Setter
+@ToString
 @AllArgsConstructor
 public class SessionServiceTableByDateResponse {
 
--- src/main/java/com/noboseki/tasktimer/playload/TaskServiceGetTaskList.java
+++ src/main/java/com/noboseki/tasktimer/playload/TaskServiceGetTaskList.java
@@ -1,12 +1,10 @@
 package com.noboseki.tasktimer.playload;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
+@ToString
 @NoArgsConstructor
 @AllArgsConstructor
 public class TaskServiceGetTaskList {
