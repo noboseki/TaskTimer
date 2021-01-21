11c05c1fe68ad4fbb5e771812c9e84096e986925
DawidW1993 <wit.dawid@interia.pl>
2021-01-21 20:07:01 +0100

    Create SessionControllerIT.java, refactor SessionService.java

--- src/main/java/com/noboseki/tasktimer/service/SessionService.java
+++ src/main/java/com/noboseki/tasktimer/service/SessionService.java
@@ -10,6 +10,7 @@ import com.noboseki.tasktimer.playload.SessionServiceChainByDateResponse;
 import com.noboseki.tasktimer.playload.SessionServiceCreateRequest;
 import com.noboseki.tasktimer.playload.SessionServiceTableByDateResponse;
 import com.noboseki.tasktimer.repository.SessionDao;
+import com.noboseki.tasktimer.service.util.ServiceUtil;
 import com.noboseki.tasktimer.service.util.SessionService.SessionServiceGetBarChainByDateUtil;
 import com.noboseki.tasktimer.service.util.SessionService.SessionServiceGetTableByDateUtil;
 import lombok.RequiredArgsConstructor;
@@ -34,6 +35,7 @@ public class SessionService {
     private final TaskService taskService;
     private final UserService userService;
     private final SessionDao sessionDao;
+    private final ServiceUtil util;
 
     public String create(User user, @Valid SessionServiceCreateRequest request) {
         Task task = taskService.findByNameAndUser(user, request.getTaskName());
@@ -48,29 +50,36 @@ public class SessionService {
         return "Session has been " + "created";
     }
 
-    public List<SessionServiceTableByDateResponse> getTableByDate(User user, LocalDate fromDate, LocalDate toDate) {
+    public List<SessionServiceTableByDateResponse> getTableByDate(User user, String fromDate, String toDate) {
         userService.findByEmile(user.getEmail());
+        checkDateFromString(fromDate, toDate);
+
+        LocalDate from = LocalDate.parse(fromDate);
+        LocalDate to = LocalDate.parse(toDate);
 
         List<Session> sessionsListBetweenDate = sessionDao.findAllByTask_UserAndDateBetween(user, Date.valueOf(fromDate), Date.valueOf(toDate));
 
         if (sessionsListBetweenDate.isEmpty()) {
-            return getTableByDateUtil.fillEmptyResponseList(fromDate, toDate);
+            return getTableByDateUtil.fillEmptyResponseList(from, to);
         }
 
-        return getTableByDateUtil.fillResponseList(sessionsListBetweenDate, fromDate, toDate);
+        return getTableByDateUtil.fillResponseList(sessionsListBetweenDate, from, to);
     }
 
-    public SessionServiceChainByDateResponse getBarChainByDate(User user, LocalDate fromDate, LocalDate toDate) {
+    public SessionServiceChainByDateResponse getBarChainByDate(User user, String fromDate, String toDate) {
         userService.findByEmile(user.getEmail());
+        checkDateFromString(fromDate, toDate);
+        LocalDate from = LocalDate.parse(fromDate);
+        LocalDate to = LocalDate.parse(toDate);
 
-        List<Session> sessionsBetweenDateForUser = sessionDao.findAllByTask_UserAndDateBetween(user, Date.valueOf(fromDate), Date.valueOf(toDate));
+        List<Session> sessionsBetweenDateForUser = sessionDao.findAllByTask_UserAndDateBetween(user, Date.valueOf(from), Date.valueOf(to));
 
         if (sessionsBetweenDateForUser.isEmpty()) {
             return SessionServiceChainByDateResponse.builder()
                     .dataList(new ArrayList<>())
-                    .dateLabel(getBarChainByDateUtil.createDateLabel(fromDate, toDate)).build();
+                    .dateLabel(getBarChainByDateUtil.createDateLabel(from, to)).build();
         }
-        return getBarChainByDateUtil.fillBarChainByDate(sessionsBetweenDateForUser, fromDate, toDate);
+        return getBarChainByDateUtil.fillBarChainByDate(sessionsBetweenDateForUser, from, to);
     }
 
     public List<GetByTaskSessionResponse> getAllByTask(User user, String taskName) {
@@ -117,6 +126,18 @@ public class SessionService {
         } catch (Exception e) {
             log.error("Time error", e);
             throw new DateTimeException("Time", time);
+        }
+    }
+
+    private boolean checkDateFromString(String from, String to) {
+        final String format = "yyyy-MM-dd";
+        boolean fromCorrect = util.isValidFormat(format, from);
+        boolean toCorrect = util.isValidFormat(format, to);
+
+        if (fromCorrect && toCorrect) {
+            return true;
+        } else {
+            throw new DateTimeException("Date", from + " or " + to);
         }
     }
 }
--- src/main/java/com/noboseki/tasktimer/service/util/ServiceUtil.java
+++ src/main/java/com/noboseki/tasktimer/service/util/ServiceUtil.java
@@ -3,6 +3,9 @@ package com.noboseki.tasktimer.service.util;
 import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Component;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
 @Component
 @NoArgsConstructor
 public class ServiceUtil {
@@ -23,5 +26,19 @@ public class ServiceUtil {
         }
 
         return time;
+    }
+
+    public boolean isValidFormat(String format, String value) {
+        java.util.Date date = null;
+        try {
+            SimpleDateFormat sdf = new SimpleDateFormat(format);
+            date = sdf.parse(value);
+            if (!value.equals(sdf.format(date))) {
+                date = null;
+            }
+        } catch (ParseException ex) {
+            ex.printStackTrace();
+        }
+        return date != null;
     }
 }
